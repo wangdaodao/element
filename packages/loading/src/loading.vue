2733695d73816499d95b82a54015eeeed3c3190e
wangdaodao <hi@wangdaodao.com>
2024-09-13 13:59:12 +0800

    fix: v-loading trigger the click event of the current element

--- packages/loading/src/loading.vue
+++ packages/loading/src/loading.vue
@@ -4,7 +4,8 @@
       v-show="visible"
       class="el-loading-mask"
       :style="{ backgroundColor: background || '' }"
-      :class="[customClass, { 'is-fullscreen': fullscreen }]">
+      :class="[customClass, { 'is-fullscreen': fullscreen }]"
+      @click.stop="(e) => e.stopPropagation()">
       <div class="el-loading-spinner">
         <svg v-if="!spinner" class="circular" viewBox="25 25 50 50">
           <circle class="path" cx="50" cy="50" r="20" fill="none"/>
